70b4a45518ee2af96239afe2e0e43c793691a2fc
Chiqueto <lfchiqueto@gmail.com>
2025-06-22 15:41:53 -0300

    fix: avaliacao get ok

--- src/main/java/com/senai/classline/repositories/AvaliacaoRepository.java
+++ src/main/java/com/senai/classline/repositories/AvaliacaoRepository.java
@@ -1,6 +1,9 @@
 package com.senai.classline.repositories;
 
 import com.senai.classline.domain.avaliacao.Avaliacao;
+import com.senai.classline.domain.disciplina.Disciplina;
+import com.senai.classline.domain.professor.Professor;
+import com.senai.classline.domain.turma.Turma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,20 +13,17 @@ import java.util.Set;
 
 public interface AvaliacaoRepository extends JpaRepository<Avaliacao, Long> {
 
-    @Query("SELECT a FROM Avaliacao a " +
-            "WHERE a.disciplina.idDisciplina = :idDisciplina " +
-            "AND EXISTS (" +
-            "  SELECT 1 FROM DisciplinaSemestre ds " +
-            "  JOIN ds.semestre.grade g " +
-            "  JOIN g.turma t " +
-            "  WHERE ds.disciplina.idDisciplina = :idDisciplina " +
-            "  AND ds.professor.idProfessor = :idProfessor " +
-            "  AND t.idTurma = :idTurma" +
-            ")")
+    List<Avaliacao> findByTurmaIdTurmaAndProfessorIdProfessorAndDisciplinaIdDisciplina(
+            Long idTurma,
+            String idProfessor,
+            Long idDisciplina
+    );
+
     List<Avaliacao> findByTurmaAndProfessorAndDisciplina(
-            @Param("idTurma") Long idTurma,
-            @Param("idProfessor") String idProfessor,
-            @Param("idDisciplina") Long idDisciplina
+            Turma turma,
+            Professor professor,
+            Disciplina disciplina
+
     );
 
     @Query("SELECT a FROM Avaliacao a " +
